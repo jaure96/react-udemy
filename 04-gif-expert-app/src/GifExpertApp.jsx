746642332a49e2d2d8f907ed1c6157d0ac7ca257
jaure96 <x.jauregi96@gmail.com>
2022-10-01 09:02:24 +0200

    gif app

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
 
-const GifExpertApp = () => {
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+  };
+
   return (
     <>
-      <div>GifExpertApp</div>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory onNewCategory={(value) => onAddCategory(value)} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
-
-export default GifExpertApp;
