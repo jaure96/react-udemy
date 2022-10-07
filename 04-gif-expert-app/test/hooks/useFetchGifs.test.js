a395960155415858611447d1970217e74afd7e6f
jaure96 <x.jauregi96@gmail.com>
2022-10-07 19:23:00 +0200

    add test to custom hook

---
+++ 04-gif-expert-app/test/hooks/useFetchGifs.test.js
@@ -0,0 +1,24 @@
+import { renderHook, waitFor } from "@testing-library/react";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+describe("Testing my custom useFetch hook", () => {
+  test("Should return the initial state", () => {
+    const { result } = renderHook(() => useFetchGifs("One Punch"));
+    const { images, isLoading } = result.current;
+
+    expect(images.length).toBe(0);
+    expect(isLoading).toBeTruthy();
+  });
+
+  test("Should return an array of images and isLoading to false", async () => {
+    const { result } = renderHook(() => useFetchGifs("One Punch"));
+
+    await waitFor(() =>
+      expect(result.current.images.length).toBeGreaterThan(0)
+    );
+    const { images, isLoading } = result.current;
+
+    expect(images.length).toBeGreaterThan(0);
+    expect(isLoading).toBeFalsy();
+  });
+});
