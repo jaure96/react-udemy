4235f3ef8dfee1ea2453dc0a1751a29ddffd8de0
jaure96 <x.jauregi96@gmail.com>
2022-10-07 19:08:59 +0200

    add test to gifsGrid

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,4 +1,5 @@
 import { GifItem } from "./GifItem";
+import PropTypes from "prop-types";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
@@ -16,4 +17,8 @@ export const GifGrid = ({ category }) => {
       </div>
     </>
   );
+};
+
+GifGrid.propTypes = {
+  category: PropTypes.string.isRequired,
 };
